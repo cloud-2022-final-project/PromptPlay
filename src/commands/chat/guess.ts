3a568b1589bcfed675336c09859cb4e82e21ea2d
extramix <sirapat.jr@gmail.com>
2023-04-09 20:52:33 +0700

    Guess slash command

--- src/commands/chat/guess.ts
+++ src/commands/chat/guess.ts
@@ -1,10 +1,12 @@
 import { ChatInputCommandInteraction, EmbedBuilder, PermissionsString } from 'discord.js';
+import { prisma } from '../../prisma.js';
 import { Lang } from '../../services/index.js';
 import { Command, CommandDeferType } from '../command.js';
 import { EventData } from '../../models/internal-models.js';
 import { InteractionUtils } from '../../utils/index.js';
 import { Language } from '../../models/enum-helpers/index.js';
 import { processingDaily } from '../../jobs/daily.js';
+import fetch from 'node-fetch';
 
 /**
  * This command handles when a player makes a guess.
@@ -28,8 +30,9 @@ export class Guess implements Command {
             return;
         }
 
-        // get the prompt
-        const prompt = intr.options.getString('prompt');
+        // get the prompt from the named option
+        console.log(intr.options);
+        const prompt = intr.options.get('prompt')?.value as string;
 
         // check if the prompt is valid
         if (!prompt) {
@@ -46,5 +49,70 @@ export class Guess implements Command {
         // TODO: do something with the prompt
 
         await InteractionUtils.send(intr, `Your prompt is "\`${prompt}\`".`);
+
+        const currentImg = await prisma.dailyImage.findFirst();
+
+        function getScore(guessedPrompt: string, truePrompt: string): Promise<number> {
+            return fetch(
+                `https://cal-score.vercel.app/api/score?a=${guessedPrompt}&b=${truePrompt}`
+            )
+                .then(async response => {
+                    if (response.ok) {
+                        return Number(((await response.json()) as number) * 100);
+                    } else {
+                        throw new Error('Network response was not ok.');
+                    }
+                })
+                .catch(error => {
+                    console.error('There was a problem with the fetch operation:', error);
+                    throw error; // rethrow the error so the caller can handle it
+                });
+        }
+
+        const currentScore = await getScore(prompt, currentImg.prompt);
+
+        //create a record in the database of User and DalilyPlayer, if not exist
+        const user = await prisma.user.findUnique({
+            where: {
+                discordId: intr.user.id,
+            },
+        });
+
+        if (!user) {
+            await prisma.user.create({
+                data: {
+                    discordId: intr.user.id,
+                    totalScore: 0,
+                },
+            });
+        }
+
+        const dailyPlayer = await prisma.dailyPlayer.findUnique({
+            where: {
+                discordId: intr.user.id,
+            },
+        });
+
+        if (!dailyPlayer) {
+            await prisma.dailyPlayer.create({
+                data: {
+                    discordId: intr.user.id,
+                    prompt: prompt,
+                    score: currentScore,
+                    dailyImageId: currentImg.url,
+                    dailyImageUrl: currentImg.url,
+                },
+            });
+        } else {
+            await prisma.dailyPlayer.update({
+                where: {
+                    discordId: intr.user.id,
+                },
+                data: {
+                    prompt: prompt,
+                    score: currentScore,
+                },
+            });
+        }
     }
 }

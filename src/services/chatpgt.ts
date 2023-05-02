b146eed41f2169d6d10ebdc45741adec4cbb2533
extramix <sirapat.jr@gmail.com>
2023-05-02 23:37:06 +0700

    change chatgpt model

--- src/services/chatpgt.ts
+++ src/services/chatpgt.ts
@@ -11,8 +11,8 @@ const openai = new OpenAIApi(configuration);
  */
 const send = async (prompt: string): Promise<string> => {
     const completion = await openai.createCompletion({
-        // model: 'text-davinci-003',
-        model: 'gpt-3.5-turbo',
+        model: 'text-davinci-003',
+        // model: 'gpt-3.5-turbo',
         prompt: prompt,
         max_tokens: 1000,
     });
